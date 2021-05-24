a849d2b1c2a43a085ddfd9468ebe022ee6896a05
DustinGentry <dustiumus@gmail.com>
2021-05-24 16:24:56 -0600

    final code

--- calc.js
+++ calc.js
@@ -14,6 +14,26 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+		console.log("Operand: ",mathSymbol);
+		console.log("First Number ",num1);
+		console.log("Second Number ",num2);
+		
+	if(mathSymbol === "+")
+		{console.log(num1+num2)}
+
+	if(mathSymbol === "-")
+	{console.log(num1-num2)};
+
+	if(mathSymbol === "*")
+	{console.log(num1*num2)};
+
+	if(mathSymbol === "/")
+     {console.log(num1/num2)};
+
+	if(mathSymbol === "root")
+	{console.log(Math.sqrt(num1))};
+	{console.log(Math.sqrt(num2))};
+
 
 	// This line closes the connection to the command line interface.
 	reader.close()
